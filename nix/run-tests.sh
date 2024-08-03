9212419823ef840e1c6a74faab5f72fd984d687e
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-08-03 12:57:46 +0300

    feat: add `flake.nix` and helpers

---
+++ nix/run-tests.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Get the directory of the current script
+SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
+
+errors=0
+
+if [ -f pyproject.toml ]; then
+    # This looks like a Python package.
+    source "$SCRIPT_DIR/install-in-virtualenv.sh"
+
+    # Run pytest if it's available.
+    if command -v pytest &> /dev/null; then
+        pytest || errors=$?
+    fi
+fi
+
+if [ -f Cargo.toml ]; then
+    cargo test || errors=$?
+fi
+
+exit $errors
