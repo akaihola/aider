b5b6ffc3019e29eda2275275cf79a4c256806fed
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2024-08-03 12:57:46 +0300

    feat: add `flake.nix` and helpers

---
+++ nix/run-lint.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+# Get the directory of the current script
+SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
+
+errors=0
+
+if [ -f pyproject.toml ]; then
+    # This looks like a Python package.
+    source "$SCRIPT_DIR/install-in-virtualenv.sh"
+
+    run() {
+        command -v "$1" && ( $@ || errors=$? )
+    }
+
+    source ${VENV}/bin/activate
+    run darker
+    run graylint
+fi
+
+for file in "$@"; do
+    case "$file" in
+        *.yml|*.yaml)
+            run yamllint "$file"
+            ;;
+        *.sh|*.md|*.rst|*.txt)
+            run codespell "$file"
+            ;;
+    esac
+done
+
+if [ -f Cargo.toml ]; then
+  rustfmt --edition=2021 "$@"
+  run cargo clippy
+fi
+
+if find -regex ".*\.\(m?j\|t\)s$" -print | grep -q .; then
+  run eslint "$@"
+fi
+
+find -name "*.nix" -exec nix-instantiate --parse {} \+ >/dev/null || errors=$?
+
+exit $errors
