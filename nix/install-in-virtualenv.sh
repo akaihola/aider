3d85a5e9fba7de40b07f83b576ec19628916a4df
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2025-01-11 18:35:51 +0200

    feat: use uv in `install-in-virtualenv.sh`

--- nix/install-in-virtualenv.sh
+++ nix/install-in-virtualenv.sh
@@ -5,16 +5,8 @@
 # It then installs the package in editable mode with all of its extras (if any).
 # This script is used in `run-lint.sh` and `run-tests.sh`.
 
-# Define locations for a virtualenv and the pip binary within.
-VENV=.venv
-PIP=${VENV}/bin/pip
-
-# Create a virtualenv if it doesn't exist and upgrade pip (to suppress upgrade warnings).
-[ ! -f ${PIP} ] && python -m venv ${VENV} && ${PIP} install -U pip
+# Create a virtualenv if it doesn't exist
+uv venv
 
 # Install the package in editable mode with all of its extras (if any).
-EXTRAS=$(\
-  ${PIP} install --quiet --report=- --editable="." \
-  | jq --raw-output '.install[0].metadata.provides_extra|join(",")' 2> /dev/null \
-)
-[ -n "${EXTRAS}" ] && ${PIP} install --editable=".[${EXTRAS}]"
+uv pip install --all-extras --editable=.
