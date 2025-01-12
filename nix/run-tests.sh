7114e1c40a05b2cadf970192d95d1f6930cfb4e2
Antti Kaihola <13725+akaihola@users.noreply.github.com>
2025-01-12 18:59:11 +0200

    feat: simplify run-lint and run-tests using uv

--- nix/run-tests.sh
+++ nix/run-tests.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-# Get the directory of the current script
-SCRIPT_DIR="${BASH_SOURCE[0]%/*}"
-
 errors=0
 
 if [ -f pyproject.toml ]; then
     # This looks like a Python package.
-    source ${INSTALL_IN_VIRTUALENV_CMD}
+    uv sync --all-groups --all-extras
+    UV_PYTHON=.venv
 
     # Run pytest if it's available.
-    if command -v pytest &> /dev/null; then
-        pytest || errors=$?
+    if uv pip show --quiet pytest; then
+        uv run pytest || errors=$?
     fi
 fi
 
